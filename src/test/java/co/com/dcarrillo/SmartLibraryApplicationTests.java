54744384da914668340a5279d573e340c12c4b26
Daniel Carrillo <daniel.carrillo01@uptc.edu.co>
2025-11-24 21:22:21 -0500

    First Commit Smart Library

---
+++ src/test/java/co/com/dcarrillo/SmartLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.dcarrillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
